3ef89e1517ae39a74c0c0ac3b30e176df861115f
Mikita <Kuznikit@gmail.com>
2021-09-15 17:29:00 +0300

    refactor+addFindElement

--- src/test/java/FindElement.java
+++ src/test/java/FindElement.java
@@ -6,9 +6,30 @@ public class FindElement extends BaseTest {
 
     @Test
     public void abd() {
-        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("login-button"));
+        driver.findElement(By.xpath("//*[text()='Sauce Labs Backpack']"));
+        driver.findElement(By.cssSelector("[class=removed_cart_item]"));
+        driver.findElement(By.cssSelector("[class=title"));
+        driver.findElement(By.id("first-name"));
+        driver.findElement(By.id("last-name"));
+        driver.findElement(By.id("postal-code"));
+        driver.findElement(By.id("continue"));
+        driver.findElement(By.cssSelector("[data-test=error]"));
+        driver.findElement(By.cssSelector(".title"));
         driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
         driver.findElement(By.id("remove-sauce-labs-backpack"));
+        driver.findElement(By.id("user-name"));
+        driver.findElement(By.id("password"));
+        driver.findElement(By.id("login-button"));
+        driver.findElement(By.cssSelector(".title"));
+        driver.findElement(By.xpath("//*[contains(text(), '%s')]/ancestor::*[contains(@class, 'inventory_item')]//button"));
+        driver.findElement(By.xpath("//*[contains(text(), '%s')]/ancestor::*[contains(@id, 'title_link')]"));
+        driver.findElement(By.id("inventory_item_container"));
+        driver.findElement(By.cssSelector("[class='product_sort_container']"));
+        driver.findElement(By.cssSelector("[data-test=error]"));
+        driver.findElement(By.cssSelector("[class=error-button]"));
+        driver.findElement(By.cssSelector("[class=error-message-container]"));
         driver.findElement(By.id("react-burger-menu-btn"));
         driver.findElement(By.cssSelector("class=[inventory_item_price]"));
         driver.findElement(By.cssSelector("[class=add-to-cart-sauce-labs-backpack]"));
@@ -17,27 +38,9 @@ public class FindElement extends BaseTest {
         driver.findElement(By.className("product_sort_container"));
         driver.findElement(By.tagName("[class='shopping_cart_badge']"));
         driver.findElement(By.name("with the sleek"));
-        driver.get("https://www.saucedemo.com/cart.html");
         driver.findElement(By.xpath("//@[id='add-to-cart-sauce-labs-backpack]"));
         driver.findElement(By.xpath("//@[id='add-to-cart-sauce-labs-backpack]"));
 
-        /*//*[@id="add-to-cart-sauce-labs-backpack"]
-
-        with the sleek
-        carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.
-        <span class="shopping_cart_badge">1</span>
-<span class="shopping_cart_badge">1</span>
-<button id="react-burger-menu-btn" style="position: absolute; left: 0px; top: 0px; z-index: 1; width: 100%; height: 100%; margin: 0px; padding: 0px; border: none; font-size: 0px; background: transparent; cursor: pointer;">Open Menu</button>
-        <select class="product_sort_container" data-test="product_sort_container"><option value="az">Name (A to Z)</option><option value="za">Name (Z to A)</option><option value="lohi">Price (low to high)</option><option value="hilo">Price (high to low)</option></select>
-        <div class="inventory_item_name">Sauce Labs Onesie</div>
-        <div class="inventory_item_name">Sauce Labs Onesie</div>
-//*[@id="item_2_title_link"]/div
-        <div class="inventory_item_name">Test.allTheThings() T-Shirt (Red)</div>
-<div class="inventory_item_price">$29.99</div>
-
-<button class="btn btn_secondary btn_small btn_inventory" data-test="remove-sauce-labs-backpack" id="remove-sauce-labs-backpack" name="remove-sauce-labs-backpack">Remove</button>
-                <button class="btn btn_primary btn_small btn_inventory" data-test="add-to-cart-sauce-labs-backpack" id="add-to-cart-sauce-labs-backpack" name="add-to-cart-sauce-labs-backpack">Add to cart</button>
-    */
     }
 
 }
